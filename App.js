1c5d47375ec0419d92d9f86039a6ebda8f9f8243
Jonathan Leonhardt <jonathan.arai@gmail.com>
2022-12-05 17:10:09 -0300

    init

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+  
+import HomeScreen from "./screens/HomeScreen";
+import UserScreen from "./screens/UserScreen";
+import SettingScreen from "./screens/SettingScreen";
+  
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    User: UserScreen,
+    Setting: SettingScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#006600",
+      },
+      headerTitleStyle: {
+        fontWeight: "bold",
+        color: "#FFF",
+      },
+      headerTintColor: "#FFF",
+    },
+  },
+  {
+    initialRouteName: "Home",
+  }
+);
+  
+const Navigator = createAppContainer(AppNavigator);
+  
+export default function App() {
+  return (
+    <Navigator>
+      <HomeScreen />
+    </Navigator>
+  );
+}
---
+++ components/header/header.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Text } from "react-native";
+
+
+const HeaderComponent = () => {
+  return (
+    <Text> Texto </Text>
+  );
+};
+
+export default HeaderComponent;
