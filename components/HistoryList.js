0049ec8e4a39cb3a251b408e1f72d004c46aaba8
Jonathan Leonhardt <jonathan.arai@syonet.com>
2022-12-06 15:00:19 -0300

    bkp

--- components/HistoryList.js
+++ components/HistoryList.js
@@ -6,30 +6,31 @@ import {
   SafeAreaView,
   SectionList,
   StatusBar,
+  Button,
 } from "react-native";
 
 const DATA = [
   {
-    title: "Main dishes",
+    title: "Today",
     data: ["Pizza", "Burger", "Risotto"],
   },
   {
-    title: "Sides",
+    title: "05 Dez",
     data: ["French Fries", "Onion Rings", "Fried Shrimps"],
   },
   {
-    title: "Drinks",
+    title: "04 Dez",
     data: ["Water", "Coke", "Beer"],
-  },
-  {
-    title: "Desserts",
-    data: ["Cheese Cake", "Ice Cream"],
-  },
+  }
 ];
 
 const Item = ({ title }) => (
   <View style={styles.item}>
     <Text style={styles.title}>{title}</Text>
+    <Button
+      title="Remove"
+      color="#841584"
+    />
   </View>
 );
 
@@ -50,19 +51,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: StatusBar.currentHeight,
-    marginHorizontal: 16,
+    marginHorizontal: 14
   },
   item: {
     backgroundColor: "#D4F1F4",
     padding: 20,
-    marginVertical: 8,
+    marginVertical: 4,
+    borderRadius: 8,
+    borderStyle: "solid",
+    borderColor: 'black'
   },
   header: {
-    fontSize: 32,
-    backgroundColor: "#75E6DA",
+    fontSize: 14
   },
   title: {
-    fontSize: 24,
+    fontSize: 18
   },
 });
 
