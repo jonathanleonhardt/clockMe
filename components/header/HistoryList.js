d6e287e7947a432b1a556dfdffb6dc582c276026
Jonathan Leonhardt <jonathan.arai@gmail.com>
2022-12-05 21:54:42 -0300

    color

--- components/header/HistoryList.js
+++ components/header/HistoryList.js
@@ -53,13 +53,13 @@ const styles = StyleSheet.create({
     marginHorizontal: 16,
   },
   item: {
-    backgroundColor: "#f9c2ff",
+    backgroundColor: "#D4F1F4",
     padding: 20,
     marginVertical: 8,
   },
   header: {
     fontSize: 32,
-    backgroundColor: "#fff",
+    backgroundColor: "#75E6DA",
   },
   title: {
     fontSize: 24,
